d4d31c84db10543c9e5a003e76440b8a8e6bcfaf
Anadian <willanad@yandex.com>
2020-06-07 04:11:52 -0400

    test: Debugging `cli_test.sh`.

--- cli_test.sh
+++ cli_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [[ './source/main.js -Vhc' ]]; then
-	cli_test_stdout=`./source/main.js --input-regex-string 'pcre/(simple)? regex/replace/vim' -o`;
+if [[ 'node source/main.js -Vhc' ]]; then
+	cli_test_stdout=`node source/main.js --input-regex-string 'pcre/(simple)? regex/replace/vim' -o`;
 	echo $cli_test_stdout
 	if [[ $cli_test_stdout == '\(simple\)\= regex' ]]; then
 		exit 0;
