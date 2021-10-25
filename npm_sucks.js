24407cbabbc9561da67386e05e6172ac4d6c826f
Anadian <willanad@yandex.com>
2021-10-24 23:10:34 -0400

    test: Full CLI testing now functional.

--- npm_sucks.js
+++
@@ -1,4 +0,0 @@
-#!/usr/local/bin/node
-const ChildProcess = require('child_process');
-var child_process_object = ChildProcess.spawnSync( 'ls' );
-console.log('%o', child_process_object);
--- npm_sucks.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-node --version 1>temp_stdout.txt;
-cat temp_stdout.txt;
-rm temp_stdout.txt;
