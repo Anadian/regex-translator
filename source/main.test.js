41796806d442e0576b20f2f8467b2dbf4ebd342f
Anadian <willanad@yandex.com>
2021-10-27 00:45:50 -0400

    Try again.

--- source/main.test.js
+++ source/main.test.js
@@ -253,10 +253,10 @@ AVA.cb( 'CLI:InputFileToOutputFile', function(t){
 	var process_object = ChildProcess.fork('source/main.js', [ '-I', 'test/input/semver_regex.txt', '-F', 'pcre', '-T', 'ecma', '-O', 'temp_out.txt' ], { silent: true });
 	var stdout_string = '';
 	var stderr_string = '';
-	var expected_output_string = FileSystem.readFileSync( 'test/DATA/EXPECTED/semver_regex.txt', 'utf8' ).replace( /\r\n/g, '\n' );
+	var expected_output_string = FileSystem.readFileSync( 'test/DATA/EXPECTED/semver_regex.txt', 'utf8' ).replace( /\r/g, '' );
 	process_object.on( 'exit', function( code, signal ){
 		console.log(`code: ${code} signal: ${signal}`);
-		var actual_output_string = FileSystem.readFileSync( 'temp_out.txt', 'utf8' ).replace( /\r\n/g, '\n' );
+		var actual_output_string = FileSystem.readFileSync( 'temp_out.txt', 'utf8' ).replace( /\r/g, '' );
 		FileSystem.unlinkSync( 'temp_out.txt' );
 		if( code === 0 ){
 			t.is( actual_output_string, expected_output_string );
